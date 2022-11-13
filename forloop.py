aa34f973c39558f714d13d00069c5c5d83f18cc5
rmechery3105 <118095026+rmechery3105@users.noreply.github.com>
2022-11-13 11:09:29 +0530

    Create forloop.py

---
+++ forloop.py
@@ -0,0 +1,14 @@
+# try for loop with one step and two steps
+
+#for i in range(10,1,-1): 
+ #   print(i)
+
+# sum of list using for
+
+int_list = [4,8,-2,3,4,1234,222]
+sum_int = 0
+for num in int_list:
+    sum_int = sum_int + num
+
+# print sum 
+print("sum of list =",sum_int)   
