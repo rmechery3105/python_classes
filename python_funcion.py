c8872a2f02df5a4cab3cad03ab3af5f3c3a563d4
rmechery3105 <118095026+rmechery3105@users.noreply.github.com>
2022-11-15 19:10:20 +0530

    Create python_funcion.py

---
+++ python_funcion.py
@@ -0,0 +1,19 @@
+# funcitons in python
+list1 = [342432,2344,22,35335,-1343]
+print('max of list1:',max(list1))
+print('min of list1:',min(list1))
+print('sum of list1:',sum(list1))
+
+# first function
+def welcome_mes():
+   # print('welcome to python programming')
+    return 'welcome to python programming'
+    
+
+
+print('simply call function')
+welcome_mes()
+stg1 = welcome_mes()
+print('value returned by function:',stg1)
+
+ # Now Playing - 13th Nov Live Class Python Part 3 watched till 2:08:48 hours 
