d8455fa1a1ba20370325bcb2ed5abe84cb6677d0
rmechery3105 <118095026+rmechery3105@users.noreply.github.com>
2022-11-21 23:30:28 +0530

    Create python_operators.py

---
+++ python_operators.py
@@ -0,0 +1,62 @@
+# show examples of operators like addition +, subraction -, float division /, multiplication *, modules %, exponent ** , integer division //
+x = 5
+y = 3
+print("addition of x and y:",x+y)
+print("subscraction of x and y:",x-y)
+print("float division of x and y:",x/y)
+print("multiplication of x and y:",x*y)
+print("modulus of x and y:",x%y)
+print("exponent  of x and y:",x**y)
+print("integer division of x and y:",x//y)
+
+# some operations on strings
+
+first_name = "rajesh"
+empty_str =''
+
+print("full name : ",first_name+" "+empty_str+"mechery")
+
+# minus of string does not work
+#print("full name : ",first_name+" " - empty_str+"mechery")
+
+#print("power of string : ",first_name**2)
+
+# multiply string
+print("power of string : ",first_name*2)
+
+# assingment operators
+print("assingment operators")
+x=5
+x+=2
+print("addition:",x)
+x-=2
+print("subscraction:",x)
+x/=2
+print("division:",x)
+x*=2
+print("multiplication:",x)
+x%=2
+print("modulus :",x)
+x**=2
+print("exponent  of x :",x)
+x//=2
+print("integer division :",x//y)
+
+a=5
+b=8
+print("a==b",a==b)
+print("a!=b",a!=b)
+print("a<b",a<b)
+print("a>b",a>b)
+print("a<=b",a<=b)
+print("a>=b",a>=b)
+
+# logical operators
+m=10
+n=2
+
+print("m==10 and n>10",m==10 and n>10)
+print("m=10 or n>10",m==10 or n>10)
+print("not(m>10 or n>10)",not(m==10 or n>10))
+
+
