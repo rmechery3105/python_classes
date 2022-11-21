35cc304afcc9148491c1348bc5f46e9f72787e5d
rmechery3105 <118095026+rmechery3105@users.noreply.github.com>
2022-11-21 23:38:42 +0530

    Create python_if_else.py

---
+++ python_if_else.py
@@ -0,0 +1,21 @@
+# if else
+
+x=10
+y=-2
+
+if x == y:
+    print("x==y")
+else:
+     print("x!=y")
+     
+
+# nested if else condition
+marks=62
+if marks>90:
+    print("grade a+")
+elif marks >=80 and marks<=90:
+    print("grade a")    
+elif marks >=70 and marks<80:
+    print("grade b")    
+else: print("grade c")    
+
